167c7097194fde5938174e1999a7abc09fb30575
Lolezhechka <lolegkropka@gmail.com>
2024-04-07 21:41:58 +0300

    4

---
+++ Lab.asm.3/Task4.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+int main()
+{
+	int a, b,a1,b1,m(1),GCD;
+	std::cin >> a1;
+	std::cin >> b1;
+	_asm {
+		mov eax,a1
+		mov ecx,-1
+		cmp eax,0
+		js negativ
+		negativ:
+		mul ecx
+		mov a1,eax
+		mov m,-1
+	}
+	_asm {
+		mov eax, a1
+		mov ebx, b1
+		loop1:
+		cmp eax, ebx
+			je done
+			jg continue
+			sub ebx, eax
+			jmp loop1
+			continue:
+		sub eax, ebx
+			jmp loop1
+			done:
+		mov GCD, eax
+	}
+	_asm {
+		mov eax,a1
+		mov ebx,GCD
+		cdq
+		div ebx
+		mov ebx,m
+		mul ebx
+		mov a,eax
+	}
+	_asm {
+		mov eax, b1
+		mov ebx, GCD
+		cdq
+		div ebx
+		mov b, eax
+	}
+	std::cout << a << std::endl << b << std::endl << m <<std::endl << GCD;
+	return 0;
+}
